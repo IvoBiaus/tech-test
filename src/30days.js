c993901b07a023a379add35ee68ba360eb8e9297
Ivo Biaus <ivobiaus@gmail.com>
2020-08-26 14:12:50 -0300

    - Updated 30days script in order to save the results in a file insted of showing it through the console.

--- src/30days.js
+++ src/30days.js
@@ -1,9 +1,19 @@
+const fs = require('fs');
 const CarInsurance = require("./carInsurance");
 const Product = require("./products/product");
 const FullCoverage = require("./products/fullCoverage");
 const SpecialFullCoverage = require("./products/specialFullCoverage");
 const MegaCoverage = require("./products/megaCoverage");
 const SuperSale = require("./products/superSale");
+
+const FILE_PATH = "result.txt";
+
+fs.unlink(FILE_PATH, (err) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+});
 
 const productsAtDayZero = [
   new Product('Medium Coverage', 10, 20),
@@ -17,14 +27,20 @@ const productsAtDayZero = [
   new SuperSale(3, 6),
 ];
 
+var stream = fs.createWriteStream(FILE_PATH, {flags:'a'});
+stream.write("*For MegaCoverage SellIn and Price will always be 0 and 80 respectively\n\n");
+
 const carInsurance = new CarInsurance(productsAtDayZero);
 const productPrinter = function (product) {
-  console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
+  const dataLine = `${product.name}, ${product.sellIn}, ${product.price}\n`;
+  stream.write(dataLine);
 };
 
 for (let i = 1; i <= 30; i += 1) {
-  console.log(`Day ${i}`);
-  console.log('name, sellIn, price');
+  stream.write(`Day ${i}\n`);
+  stream.write('name, sellIn, price\n');
   carInsurance.updatePrice().forEach(productPrinter);
-  console.log('');
+  stream.write("\n");
 }
+
+stream.end();
